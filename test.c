ec99b7b8169b83ef41bf8b8055654cd4a51695e3
wanderersean <652964528@qq.com>
2017-11-20 23:28:47 +0800

    c test

---
+++ test.c
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: ma6174
+	> Mail: ma6174@163.com 
+	> Created Time: 2017年11月20日 星期一 23时26分15秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include <stdlib.h>
+void fun1(void)
+{
+	printf("nihao");
+}
+int main()
+{
+	printf("helloworld");
+	fun1();
+	return 0;
+}
